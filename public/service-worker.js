ceae004a2f8c7820513a204e8c5b82172e84cfab
Samuel Goldfarb <shgoldfarb@uc.cl>
2018-09-24 04:15:03 -0300

    add colors

--- public/service-worker.js
+++ public/service-worker.js
@@ -7,6 +7,7 @@ var filesToCache = [
   '/index.html',
   '/scripts/app.js',
   '/styles/inline.css',
+  '/styles/materialize.css',
   '/images/ic_add_white_24px.svg',
   '/images/ic_refresh_white_24px.svg',
 ];
